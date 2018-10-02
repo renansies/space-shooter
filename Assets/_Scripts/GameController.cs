17220d9672393f1b01ca3bf5e8ed34fcd90ff47c
Renan Sies <renansies@gmail.com>
2018-10-02 15:31:42 -0300

    Explosions and Game Controller

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace _Scripts
+{
+	public class GameController : MonoBehaviour
+	{
+
+		public GameObject Hazard;
+		public Vector3 SpawnValues;
+
+		private void Start()
+		{
+			SpawnWaves();
+		}
+
+		void SpawnWaves()
+		{
+			var spawnPosition = new Vector3(Random.Range(-SpawnValues.x, +SpawnValues.x), SpawnValues.y, SpawnValues.z);
+			var spawnRotation = Quaternion.identity;
+			Instantiate(Hazard, spawnPosition, spawnRotation);
+		}
+	}
+}
